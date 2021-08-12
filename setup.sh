d6ac210bca4cdc052e2da3f11a29da72e37fce6d
amagnan <a.magnan@imperial.ac.uk>
2021-08-12 15:58:50 +0100

    adding setup file with working root version

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+alias setupATLAS='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
+setupATLAS
+lsetup "root 6.10.06-x86_64-slc6-gcc62-opt"
